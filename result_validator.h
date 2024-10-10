3fd50901137b3fca3de4264b6ec923f566f8491f
Jaume Tello Viñas <80787478+HappyMistery@users.noreply.github.com>
2024-10-10 02:31:26 +0200

    .gitignore actualitzaat

---
+++ result_validator.h
@@ -0,0 +1,12 @@
+#ifndef RESULT_VALIDATOR_H
+#define RESULT_VALIDATOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h> // Needed for pow function
+
+/* Function to compare results with expected values */
+int validate_results(const char *output_file);
+
+#endif // RESULT_VALIDATOR_H
